14fdd84338fa614bb95ac38eaa4729c8461808a7
Parul Aggarwal <parulaggarwal@Paruls-MacBook-Pro.local>
2020-10-18 16:41:18 +0530

    Fixed Shivam's code to OO code

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -1,27 +1,27 @@
 package org.oop;
 
-public class Point {
+public class Point{
     private double x;
     private double y;
 
-    public double getX() {
-        return x;
+    private double xDistance (Point finalPoint) {
+        return (finalPoint.x - this.x);
     }
 
-    public void setX(double x) {
-        this.x = x;
+    private double yDistance (Point finalPoint) {
+        return (finalPoint.y - this.y);
     }
 
-    public double getY() {
-        return y;
+    public Point(double xCoordinate, double yCoordinate) {
+        this.x = xCoordinate;
+        this.y = yCoordinate;
     }
 
-    public void setY(double y) {
-        this.y = y;
+    public double distance(Point finalPoint) {
+        return Math.sqrt(Math.pow(this.xDistance(finalPoint), 2) + Math.pow(this.yDistance(finalPoint), 2));
     }
 
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
+    public double direction(Point finalPoint) {
+        return Math.atan2(this.yDistance(finalPoint), this.xDistance(finalPoint));
     }
 }
